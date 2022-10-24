c647bd37172798c4cdcacea9a97b378b9c019a98
Emiliano Blanco <emilianoblanco@gmail.com>
2022-10-24 13:50:55 -0300

    Creo el script para conectarnos a la fuente de datos

---
+++ script.py
@@ -0,0 +1,20 @@
+#Importo librerias necesarias para hacer web scraping
+
+from bs4 import BeautifulSoup
+import requests
+
+## Script para conectarnos  a la fuente de datos
+
+web = "https://www.musimundo.com/informatica/notebook/c/98" # Esta es la web de donde vamos a sacar los datos
+
+respuesta = requests.get(web)   # Todo lo que se extrajo se guarda en la variable respuesta
+
+contenido = respuesta.text  # El contenido de respuesta se transforma a texto y se guarda en contenido
+
+## Ahora empezamos a usar BS4
+
+soup = BeautifulSoup(contenido, 'lxml')  ## Se parsean los datos
+
+print(soup.prettify()) ## Todo listo para mostrar en formato HTML para luego sacar los datos que nos interesan
+
+## Fin del script
